751dca86e25dd07b6f2474a884cf8bccb4d084b5
Florent <grenaille.florent85@gmail.com>
2018-10-27 20:31:43 +0200

    ajout section 'label'

--- test_lib_Tkinter.py
+++ test_lib_Tkinter.py
@@ -10,6 +10,8 @@ fenetre = Tk()
 label = Label(fenetre, text="Hello World !!!", bg="yellow")
 label.pack()
 
-
+# Bouton de sortie
+bouton = Button(fenetre, text="Fermer", command=fenetre.quit)
+bouton.pack()
 
 fenetre.mainloop()
