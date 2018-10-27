02c6cbde7c5b5600837722c55771cd701fe18641
Florent <grenaille.florent85@gmail.com>
2018-10-27 20:33:10 +0200

    ajout section 'radio bouton'

--- test_lib_Tkinter.py
+++ test_lib_Tkinter.py
@@ -18,4 +18,15 @@ bouton.pack()
 check = Checkbutton(fenetre, text="Nouveau??")
 check.pack()
 
+# Radio bouton / radiobutton
+# Les boutons radio sont des cases à cocher qui sont dans un groupe et dans ce groupe seul un élément peut être sélectionné.
+value = StringVar()
+bouton1 = Radiobutton(fenetre, text="Oui", variable=value, value=1)
+bouton2 = Radiobutton(fenetre, text="Non", variable=value, value=2)
+bouton3 = Radiobutton(fenetre, text="Peut être", variable=value, value=3)
+bouton1.pack()
+bouton2.pack()
+bouton3.pack()
+
+
 fenetre.mainloop()
