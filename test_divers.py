98c67f4bd05560346fb7aac07c003edefa96d389
Florent <grenaille.florent85@gmail.com>
2018-11-19 21:50:39 +0100

    ajout d'un fichier pour faire divers test

--- test_divers.py
+++ test_divers.py
@@ -6,16 +6,16 @@ from tkinter import *
 fenetre = Tk()
 
 def callback():
-    print("clique-moi")
+    print("tu as click ;) ")
     new_fenetre = Tk()
-    label2 = Label(new_fenetre, text="bien jouer", bg="yellow")
+    label2 = Label(new_fenetre, text="bien jouer", bg="#88b4fc")
     label2.pack()
     new_fenetre.mainloop()
 
-label = Label(fenetre, text="Test de fonction", bg="white")
+label = Label(fenetre, text="Test de fonction", bg="#cdfc88")
 label.pack()
 
-bouton = Button(fenetre, text="test", command=callback)
+bouton = Button(fenetre, text="action", command=callback)
 bouton.pack()
 
 bouton2 = Button(fenetre, text="quit", command=fenetre.quit)
