2e96f09f56c8c270048e7cd7091530f32b43e7d2
Florent <grenaille.florent85@gmail.com>
2018-11-19 22:12:52 +0100

    essai avec de la lecture de fichiers .txt via un bouton

--- test_divers.py
+++ test_divers.py
@@ -5,6 +5,13 @@ from tkinter import *
 
 fenetre = Tk()
 
+data_file = open("/home/florent/Documents/Script_Réalisé/test_read_write.txt", "r")
+
+def lectureDeFichiers():
+    label3 = Label(fenetre, text=data_file.read(), bg="#61ff4c")
+    label3.pack()
+    data_file.close()
+
 def callback():
     print("tu as click ;) ")
     new_fenetre = Tk()
@@ -12,14 +19,17 @@ def callback():
     label2.pack()
     new_fenetre.mainloop()
 
+
 label = Label(fenetre, text="Test de fonction", bg="#cdfc88")
 label.pack()
 
-bouton = Button(fenetre, text="action", command=callback)
+bouton = Button(fenetre, text="action", bg="#c45819", command=callback)
 bouton.pack()
 
 bouton2 = Button(fenetre, text="quit", command=fenetre.quit)
 bouton2.pack()
 
-fenetre.mainloop()
+bouton3 = Button(fenetre, text="Lecture de fichiers", command=lectureDeFichiers)
+bouton3.pack()
 
+fenetre.mainloop()
